c1592fe90f2bbb2cb0cc596accbf2c3d1c305c20
blackwut <albertoottimo@gmail.com>
2018-09-11 19:39:33 +0200

    Update and Bugfix

--- common.hpp
+++ common.hpp
@@ -2,124 +2,157 @@
 #define __COMMON_HPP__
 
 #include <iostream>
-#include <string>
-#include <cstring>
 #include <iomanip>
+#include <stdint.h>
+#include <cstring>
+#include <string>
 #include <ctime>
 #include <chrono>
+#include <vector>
 
 using namespace std;
 
+#define MAX_LEN 256
+static uint64_t randomSeed = time(0);
+
 enum ERROR {
-	EXIT_PARSE_INT = 32,
-	EXIT_PARSE_FLOAT,
-	EXIT_PARSE_STRING,
+    EXIT_ARGUMENTS_NUMBER = -1,
+    EXIT_PARSE_ARG = -32,
     EXIT_LOAD_TSP_FILE,
     EXIT_EDGE_WEIGHT_TYPE,
     EXIT_EDGE_WEIGHT_FORMAT,
     EXIT_NODE_COORD_TYPE
 };
 
-#define fitness(a, b)	aco->fitness[a * aco->nCities + b]
-#define p(a, b)			aco->p[a * aco->nCities + b]
-#define visited(a, b)	aco->visited[a * aco->nCities + b]
-#define tabu(a, b)		aco->tabu[a * aco->nCities + b]
-
-#define edges(a, b)		tsp->edges[a * tsp->dimension + b]
-
-
-#define MAX_LEN 256
-
 template <typename T>
-T parseArg(char * arg) {
+inline T parseArg(char * arg) {
     clog << "Error: type not supported!" << endl;
+    exit(EXIT_PARSE_ARG);
 }
 
-template<> uint32_t parseArg(char * arg) {
+template<>
+inline uint32_t parseArg(char * arg) {
     return atoi(arg);
 }
 
-template<> float parseArg(char * arg) {
+template<>
+inline float parseArg(char * arg) {
     return atof(arg);
 }
 
-template<> double parseArg(char * arg) {
+template<>
+inline double parseArg(char * arg) {
     return atof(arg);
-}
-
-void intArg(int argc, char * argv[], int i, int * val) {
-    if (argc > i) {
-        *val = atoi(argv[i]);
-        return;
-    }
-    clog << "Error while parsing argv[" << i << "] int value" << endl; 
-    exit(EXIT_PARSE_INT);
 }
 
 template <typename T>
-void fltArg(int argc, char * argv[], int i, T * val) {
-    if (argc > i) {
-        *val = atof(argv[i]);
-        return;
-    }
-    clog << "Error while parsing argv[" << i << "] float value" << endl; 
-    exit(EXIT_PARSE_FLOAT);
-}
-
-void strArg(int argc, char * argv[], int i, char * val) {
-    if (argc > i) {
-        strcpy(val, argv[i]);
-        return;
-    }
-    clog << "Error while parsing argv[" << i << "] string" << endl; 
-    exit(EXIT_PARSE_STRING);
-}
-
-template <typename T> void printMatrix(string name, T * matrix, int rows, int cols) {
+inline void printMatrix(string name, T * matrix, uint32_t rows, uint32_t cols) {
 
     cout << "**** " << name << " ****" << endl;
-    for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < cols; ++j) {
+    for (uint32_t i = 0; i < rows; ++i) {
+        for (uint32_t j = 0; j < cols; ++j) {
             cout << setw(3) << setprecision(3) << fixed << matrix[i * cols + j] << " ";
         }
         cout << endl;
     }
 }
 
+template <typename T>
+inline void printMatrixV(const string name,
+                  const std::vector<T> & matrix,
+                  const uint32_t rows,
+                  const uint32_t cols,
+                  const uint32_t precision)
+{
+    std::cout << "**** " << name << " ****" << std::endl;
+    for (uint32_t i = 0; i < rows; ++i) {
+        for (uint32_t j = 0; j < cols; ++j) {
+            std::cout << std::setw(precision) << std::setprecision(precision) << std::fixed << matrix[i * cols + j] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+template <typename T>
+inline bool compareArray(const string & name, T * left, T * right, uint32_t elems) {
+    for (uint32_t i = 0; i < elems; ++i) {
+        if (left[i] != right[i]) {
+            std::clog << "Error comparing array " << name << " at index ( " << i << " )" << std::endl;
+            return false;
+        }
+    }
+    return true;
+}
+
+template <typename T>
+inline bool compareMatrix(const string & name, T * left, T * right, uint32_t rows, uint32_t cols) {
+    for (uint32_t i = 0; i < rows; ++i) {
+        for (uint32_t j = 0; j < cols; ++j) {
+            if (left[i * cols + j] != right[i * cols + j]) {
+                std::clog << "Error comparing matrix " << name << " at index ( " << i << ", " << j << " )" << std::endl;
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
 static std::chrono::high_resolution_clock::time_point startPoint;
 static std::chrono::high_resolution_clock::time_point endPoint;
 
-void startTimer() {
+inline void startTimer() {
     startPoint = std::chrono::high_resolution_clock::now();
 }
 
-void stopTimer() {
+inline void stopTimer() {
     endPoint = std::chrono::high_resolution_clock::now();
 }
 
-long getTimerMS() {
+inline long getTimerMS() {
     return std::chrono::duration_cast<std::chrono::milliseconds>(endPoint - startPoint).count();
 }
 
-long getTimerUS() {
+inline long getTimerUS() {
     return std::chrono::duration_cast<std::chrono::microseconds>(endPoint - startPoint).count();
 }
 
-void printTimer() {
+inline void printTimer() {
     long msec = getTimerMS();
     long usec = getTimerUS();
     cout << "Compute time: " << msec << " ms " << usec << " usec " << endl;
 }
 
-void stopAndPrintTimer() {
+inline void stopAndPrintTimer() {
     stopTimer();
     printTimer();
 }
 
-void stopAndPrintTimer(string name) {
+inline void stopAndPrintTimer(string name) {
     stopTimer();
     cout << name;
     printTimer();
+}
+
+inline void printResult(const string & name,
+                        const uint32_t mapWorkers,
+                        const uint32_t farmWorkers,
+                        const uint32_t maxEpoch,
+                        const long     timeMS,
+                        const long     timeUS,
+                        const float    bestTourLength,
+                        const bool     checkPath)
+{
+#define LOG_SEP " "
+    std::clog << " *** "       << LOG_SEP
+    << name                    << LOG_SEP
+    << mapWorkers              << LOG_SEP
+    << farmWorkers             << LOG_SEP
+    << maxEpoch                << LOG_SEP
+    << timeMS                  << LOG_SEP
+    << timeUS                  << LOG_SEP
+    << bestTourLength          << LOG_SEP
+    << (checkPath ? "Y" : "N") << LOG_SEP
+    << std::endl;
 }
 
 #endif
