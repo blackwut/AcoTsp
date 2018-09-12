3562cb2c30280ca6bf2c2ae95d7df6d376098992
blackwut <albertoottimo@gmail.com>
2018-09-12 12:22:37 +0200

    Update

--- script.sh
+++ script.sh
@@ -1,57 +1,62 @@
 #!/bin/bash
-#To lunch script in background and exiting from ssh to avoid termination
-#nohup ./script &> log.txt &
+#Use screen to launch script
+# screen -S name                        // create new session
+# screen -r name                        // attach to a session
+
+# inside screen
+# ./script 1> /dev/null 2> log.txt &
+# CTRL + A then D                       // detach from screen session
 
 make acocpu
 
 tspBase="tsp/"
 
 tspArray=(
-	"bays29.tsp"
-	"d198.tsp"
-	"pcb442.tsp"
-	"rat783.tsp"
-	"pr1002.tsp"
-	"pcb1173.tsp"
-	"rl1889.tsp"
-	"pr2392.tsp"
-	"fl3795.tsp"
+    "bays29.tsp"
+    "d198.tsp"
+    "pcb442.tsp"
+    "rat783.tsp"
+    "pr1002.tsp"
+    "pcb1173.tsp"
+    "rl1889.tsp"
+    "pr2392.tsp"
+    "fl3795.tsp"
 )
 
 mapThreads=(
-	1
-	2
-	4
-	8
-	16
-	32
-	64
+    1
+    2
+    4
+    8
+    16
+    32
+    64
 )
 
 farmThreads=(
-	1
-	2
-	4
-	8
-	16
-	32
-	64
+    1
+    2
+    4
+    8
+    16
+    32
+    64
 )
 
 for problem in "${tspArray[@]}"
 do
-	echo $problem;
-	for i in "${mapThreads[@]}"
-	do
-		for j in "${farmThreads[@]}"
-		do
-			for k in `seq 1 10`;
-			do
-	#		./acocpu file.tsp			alpha	beta   	q	rho	maxEpoch 	mapThreads	farmThreads
-			./acocpu $tspBase$problem	1		2		1	0.5	16			$i			$j
-			done
-		done
-	done
+    echo $problem;
+    for i in "${mapThreads[@]}"
+    do
+        for j in "${farmThreads[@]}"
+        do
+            for k in `seq 1 10`;
+            do
+    #       ./acocpu file.tsp           alpha   beta    q   rho maxEpoch    mapThreads  farmThreads
+            ./acocpu $tspBase$problem   1       2       1   0.5 16          $i          $j
+            done
+        done
+    done
 done
 
 
