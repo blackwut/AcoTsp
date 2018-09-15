1a65cc3a2a3fcabd042ad2ff0759fe8615de61be
blackwut <albertoottimo@gmail.com>
2018-09-15 19:36:58 +0200

    Updated with 0 nThreads

--- script.sh
+++ script.sh
@@ -7,7 +7,7 @@
 # ./script 1> /dev/null 2> log.txt &
 # CTRL + A then D                       // detach from screen session
 
-make clean && make acocpu
+make clean && make acocpu -j4
 
 tspBase="tsp/"
 
@@ -24,6 +24,7 @@ tspArray=(
 )
 
 nThreads=(
+    0
     1
     2
     4
@@ -43,9 +44,54 @@ do
         for k in `seq 1 10`;
         do
 #       ./acocpu file.tsp           alpha   beta    q   rho maxEpoch    mapThreads  farmThreads
+            if 
         ./acocpu $tspBase$problem   1       2       1   0.5 10          $i          $i
         done
     done
 done
 
 
+
+# PARALLEL AND SEQUENTIAL VERSION
+# THERE IS THE ISSUE OF OVERLAPPING COUTs
+
+# threadsParallel=(
+#     0
+#     1
+#     2
+#     4
+#     8
+# )
+
+# threadsSequential=(
+#     16
+#     32
+#     64
+#     128
+# )
+
+# for problem in "${tspArray[@]}"
+# do
+#     for i in "${threadsParallel[@]}"
+#     do
+#         for k in `seq 1 10`
+#         do
+# #       ./acocpu file.tsp           alpha   beta    q   rho maxEpoch    mapThreads  farmThreads
+#         ./acocpu $tspBase$problem   1       2       1   0.5 10          $i          $i        1> "/dev/null" 2>> log.txt &
+#         pids[${i}]=$!
+#         done
+#         # wait for all pids
+#         for pid in ${pids[*]}; do
+#             wait $pid
+#         done
+#     done
+
+#     for i in "${threadsParallel[@]}"
+#     do
+#         for k in `seq 1 10`
+#         do
+# #       ./acocpu file.tsp           alpha   beta    q   rho maxEpoch    mapThreads  farmThreads
+#         ./acocpu $tspBase$problem   1       2       1   0.5 10          $i          $i        1> "/dev/null" 2>> log.txt
+#         done
+#     done
+# done
