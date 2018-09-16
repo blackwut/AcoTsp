66f7763ceee6b3f223767ce34a0c8666165a68a5
blackwut <albertoottimo@gmail.com>
2018-09-16 18:14:31 +0200

    Updated for final report

--- scriptGpu.sh
+++ scriptGpu.sh
@@ -6,26 +6,28 @@ tspBase="tsp/"
 
 tspArray=(
     "bays29.tsp"
-    "d198.tsp"
-    "pcb442.tsp"
-    "rat783.tsp"
-    "pr1002.tsp"
-    "pcb1173.tsp"
-    "rl1889.tsp"
-    "pr2392.tsp"
-    "fl3795.tsp"
+    # "d198.tsp"
+    # "pcb442.tsp"
+    # "rat783.tsp"
+    # "pr1002.tsp"
+    # "pcb1173.tsp"
+    # "rl1889.tsp"
+    # "pr2392.tsp"
+    # "fl3795.tsp"
 )
 
-nWarpsPerBlock=(
+nBlocks=(
     1
     2
     4
     8
     16
     32
+    64
+    128
 )
 
-nAntsPerWarp=(
+nWarpsPerBlock=(
     1
     2
     4
@@ -38,14 +40,14 @@ nAntsPerWarp=(
 for problem in "${tspArray[@]}"
 do
     echo $problem;
-    for i in "${nWarpsPerBlock[@]}"
+    for i in "${nBlocks[@]}"
     do
-        for j in "${nAntsPerWarp[@]}"
+        for j in "${nWarpsPerBlock[@]}"
         do
             for k in `seq 1 10`;
             do
-    #       ./acogpu file.tsp           alpha   beta    q   rho maxEpoch    threadsPerBlock nWarpsPerBlock nAntsPerWarp
-            ./acogpu $tspBase$problem   1       2       1   0.5 10          128             $i             $j
+    #       ./acogpu file.tsp           alpha   beta    q   rho maxEpoch    threadsPerBlock nBlocks nWarpsPerBlock
+            ./acogpu $tspBase$problem   1       2       1   0.5 10          128             $i      $j
             done
         done
     done
